9ea9408565939573b98fdae2795804bdd2b4ce5b
Renan Silva <renansantos.silva04@outlook.com>
2023-11-21 14:40:16 -0300

    Manages player information and actions

---
+++ Player.py
@@ -0,0 +1,9 @@
+'''
+Description:
+- Defines a class to represent players, storing player details,
+    and providing methods for players within in game.
+'''
+
+# class Player():
+class Player:
+    pass

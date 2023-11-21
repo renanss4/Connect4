814a23f179c44df9c331201e958d660d636d9c3c
Renan Silva <renansantos.silva04@outlook.com>
2023-11-21 14:41:56 -0300

    Orchestrates program execution and the main game loop

---
+++ Main.py
@@ -0,0 +1,8 @@
+"""
+Description:
+- Manages the overall program flow by creating instances of players,
+    the judge, and the board, and controlling the main game loop.
+"""
+
+class Main:
+    pass

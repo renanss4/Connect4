ad2fc38db55b1957f7dc70fcd453f856eadd8c9f
Renan Silva <renansantos.silva04@outlook.com>
2023-11-22 09:35:59 -0300

    Starting the game logic

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+from classes.Board import Board
+from classes.Player import Player
+
 """
 Description:
 - Manages the overall program flow by creating instances of players,
@@ -5,4 +8,28 @@ Description:
 """
 
 class Main:
-    pass
+    def __init__(self):
+        self.board = Board()
+        self.players = [Player("Player 1", "X"), Player("Player 2", "O")]
+        self.current_player_index = 0
+
+    def switch_player(self):
+        # self.current_player_index = 1
+        self.current_player_index = 1 - self.current_player_index
+
+    def run(self):
+        runnng = True
+
+        while runnng:
+            self.board.print_board()
+            current_player = self.players[self.current_player_index] # 0
+            current_player.make_move(self.board)
+
+
+            self.switch_player()
+
+
+if __name__ == "__main__":
+    game = Main()
+    game.run()
+

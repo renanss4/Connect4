095383bd2555c15b5a98c0cd85491d28414ae54d
Renan Silva <renansantos.silva04@outlook.com>
2023-11-24 13:13:54 -0300

    refactoring the CpuPlayer class, but it should improve

--- classes/Player.py
+++ classes/Player.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 '''
 Description:
@@ -14,21 +14,53 @@ class Player:
         self.is_human = is_human
 
     def make_move(self, board):
-        column = int(input(f"{self.name}, choose a column (0-{board.columns - 1}): "))
-        row = board.drop_piece(column, self.symbol)
-        if row is not None:
-            return
+        column = int(input(f"{self.name}, choose a column (1-{board.columns }): "))
+        board.drop_piece(column-1, self.symbol)
         
 class CpuPlayer(Player):
     def __init__(self, symbol='O'):
         super().__init__('CPU', symbol, False)
 
-    def make_move(self, board):
-        available_columns = []
-        for col in board.grid[0]:
-            if col == ' ':
-                available_columns.append(col)
+    def get_priority_columns(self, board):
+        priority_columns = []
 
-        if available_columns:
-            column = random.choice(available_columns)
-            board.drop_piece(column, self.symbol)
+        col = 0
+        while col < len(board.grid[0]):
+            if board.grid[0][col] == self.symbol:
+                priority_columns.append(col)
+            elif col + 1 < len(board.grid[0]) and board.grid[0][col + 1] == self.symbol:
+                priority_columns.append(col + 1)
+
+            col += 1
+
+        # sempre finaliza vazio
+
+    def make_move(self, board):
+        priority_columns = self.get_priority_columns(board)
+
+        if priority_columns:
+            chosen_column = choice(priority_columns)
+            row = len(board.grid) - 1
+
+            while row >= 0 and board.grid[row][chosen_column] != ' ':
+                row -= 1
+
+            if row >= 0:
+                board.drop_piece(chosen_column, self.symbol)
+        else:
+            available_columns = []
+            col = 0
+            while col < len(board.grid[0]):
+                if board.grid[0][col] == ' ':
+                    available_columns.append(col)
+                col += 1
+
+            if available_columns:
+                chosen_column = choice(available_columns)
+                row = len(board.grid) - 1
+
+                while row >= 0 and board.grid[row][chosen_column] != ' ':
+                    row -= 1
+
+                if row >= 0:
+                    board.drop_piece(chosen_column, self.symbol)

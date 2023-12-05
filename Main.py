a6d678a177be7fcc259528f266b8e8f20c6756a8
Renan Silva <renansantos.silva04@outlook.com>
2023-12-05 08:29:43 -0300

    Added Main

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-from classes.Board import Board
 from classes.Judge import Judge
 
 def run():
@@ -8,7 +7,6 @@ def run():
     Executes the main game loop, allowing players to take turns until there is a winner or draw.
     Prints the current state of the board after each turn.
     """
-    board = Board()
     judge = Judge()
     players = judge.create_players()
 
@@ -17,24 +15,24 @@ def run():
 
     while running:
         # Print the current state of the board
-        board.print_board()
+        judge.create_board()
 
         # Get the current player and make their move
         current_player = players[current_player_index]
 
         if current_player.is_human:
-            judge.validate_move(current_player, board)  # Validate the move using Judge
+            judge.validate_move(current_player)  # Validate the move using Judge
         else:
             # CPU player makes a move
-            current_player.make_move(board)
+            current_player.make_move(judge.board)
 
         # Check for a winner or draw
-        if judge.check_winner(board, current_player.symbol):
-            board.print_board()
+        if judge.check_winner(current_player.symbol):
+            judge.create_board()
             print(f'{current_player.name} wins! Congratulations!')
             running = False
-        elif judge.check_draw(board):
-            board.print_board()
+        elif judge.check_draw():
+            judge.create_board()
             print(f"It's a Draw!")
             running = False
 

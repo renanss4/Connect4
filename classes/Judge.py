7e935641f7112c6ec825d3c97fb7d0c8c62d8d98
Renan Silva <renansantos.silva04@outlook.com>
2023-12-05 09:07:02 -0300

    Refactoring

--- classes/Judge.py
+++ classes/Judge.py
@@ -1,3 +1,4 @@
+from classes.Board import Board
 from classes.Player import Player, CpuPlayer
 
 class Judge:
@@ -6,16 +7,31 @@ class Judge:
     or a draw in a Connect Four game played on a specified game board.
 
     Attributes:
-    - name (str): The name of the game judge.
+    - board (Board): The game board.
 
     Methods:
+    - create_board(): Creates a new game board and prints its initial state.
     - create_players(): Creates a list of players based on user input.
-    - check_winner(board, symbol): Checks if the specified symbol has won the game.
-    - check_draw(board): Checks if the game is a draw.
+    - check_winner(symbol): Checks if the specified symbol has won the game.
+    - check_draw(): Checks if the game is a draw.
     - switch_player(current_player_index): Switches the current player index for alternating turns.
-    - validate_move(current_player, board): Validates the current player's move and updates the game board.
+    - validate_move(current_player): Validates the current player's move and updates the game board.
     """
-        
+    def __init__(self):
+        """
+        Initialize the Judge instance.
+
+        Attributes:
+        - board (Board): The game board.
+        """
+        self.board = Board()
+
+    def create_board(self):
+        """
+        Creates a new game board and prints its initial state.
+        """
+        return self.board.print_board()
+
     def create_players(self):
         """
         Create players based on user input.
@@ -36,12 +52,11 @@ class Judge:
                 print('Only one or two players can play.')
         return players
 
-    def check_winner(self, board, symbol):
+    def check_winner(self, symbol):
         """
         Check if the specified symbol has won the Connect Four game.
 
         Args:
-        - board (Board): The game board.
         - symbol (str): The symbol representing the player (e.g., 'X' or 'O').
 
         Returns:
@@ -49,60 +64,57 @@ class Judge:
         """
         # Check for a horizontal win
         row = 0
-        while row < board.rows:
+        while row < self.board.rows:
             col = 0
-            while col < board.columns - 3:
-                if board.grid[row][col] == symbol and board.grid[row][col + 1] == symbol and board.grid[row][col + 2] == symbol and board.grid[row][col + 3] == symbol:
+            while col < self.board.columns - 3:
+                if self.board.grid[row][col] == symbol and self.board.grid[row][col + 1] == symbol and self.board.grid[row][col + 2] == symbol and self.board.grid[row][col + 3] == symbol:
                     return True
                 col += 1
             row += 1
 
         # Check for a vertical win
         row = 0
-        while row < board.rows - 3:
+        while row < self.board.rows - 3:
             col = 0
-            while col < board.columns:
-                if board.grid[row][col] == symbol and board.grid[row + 1][col] == symbol and board.grid[row + 2][col] == symbol and board.grid[row + 3][col] == symbol:
+            while col < self.board.columns:
+                if self.board.grid[row][col] == symbol and self.board.grid[row + 1][col] == symbol and self.board.grid[row + 2][col] == symbol and self.board.grid[row + 3][col] == symbol:
                     return True
                 col += 1
             row += 1
 
         # Check for a diagonal win (from top-left to bottom-right)
         row = 0
-        while row < board.rows - 3:
+        while row < self.board.rows - 3:
             col = 0
-            while col < board.columns - 3:
-                if board.grid[row][col] == symbol and board.grid[row + 1][col + 1] == symbol and board.grid[row + 2][col + 2] == symbol and board.grid[row + 3][col + 3] == symbol:
+            while col < self.board.columns - 3:
+                if self.board.grid[row][col] == symbol and self.board.grid[row + 1][col + 1] == symbol and self.board.grid[row + 2][col + 2] == symbol and self.board.grid[row + 3][col + 3] == symbol:
                     return True
                 col += 1
             row += 1
 
         # Check for a diagonal win (from bottom-left to top-right)
         row = 3
-        while row < board.rows:
+        while row < self.board.rows:
             col = 0
-            while col < board.columns - 3:
-                if board.grid[row][col] == symbol and board.grid[row - 1][col + 1] == symbol and board.grid[row - 2][col + 2] == symbol and board.grid[row - 3][col + 3] == symbol:
+            while col < self.board.columns - 3:
+                if self.board.grid[row][col] == symbol and self.board.grid[row - 1][col + 1] == symbol and self.board.grid[row - 2][col + 2] == symbol and self.board.grid[row - 3][col + 3] == symbol:
                     return True
                 col += 1
             row += 1
 
         return False
 
-    def check_draw(self, board):
+    def check_draw(self):
         """
         Check if the Connect Four game is a draw.
-
-        Args:
-        - board (Board): The game board.
 
         Returns:
         - bool: True if the game is a draw, False otherwise.
         """
         # Check for a draw by examining the top row of the board
         col = 0
-        while col < board.columns:
-            if board.grid[0][col] == ' ':
+        while col < self.board.columns:
+            if self.board.grid[0][col] == ' ':
                 return False
             col += 1
         return True
@@ -119,23 +131,22 @@ class Judge:
         """
         return 1 - current_player_index
 
-    def validate_move(self, current_player, board):
+    def validate_move(self, current_player):
         """
         Validate the current player's move and update the game board.
 
         Args:
         - current_player (Player): The player making the move.
-        - board (Board): The game board.
 
         Returns:
-        - bool: True if the move is valid, False otherwise.
+        - None
         """
         valid_move = False
         while not valid_move:
             column = current_player.make_move()  # Use make_move from current player
-            if 0 <= column < board.columns:
-                if board.grid[0][column] == ' ':
-                    board.drop_piece(column, current_player.symbol)
+            if 0 <= column < self.board.columns:
+                if self.board.grid[0][column] == ' ':
+                    self.board.drop_piece(column, current_player.symbol)
                     valid_move = True
                 else:
                     print('Column is full. Please choose a different column.')

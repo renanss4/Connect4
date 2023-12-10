bb3fffbf76f3a9036c9d872517f48eedecf668d6
Renan Silva <renansantos.silva04@outlook.com>
2023-12-09 23:32:33 -0300

    Adding a test

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,4 +1,14 @@
 import pytest
+from unittest.mock import patch
+from classes.Player import Player
 
-def test_pytest(x=1, y=0):
-    assert x + y == 1
+@patch('builtins.input', lambda _: '4')
+def test_make_move():
+    # Cria uma instância do jogador com um símbolo fictício (por exemplo, 'X')
+    player = Player("Player1", "X")
+
+    # Chama o método make_move com um argumento fictício (None neste caso)
+    column = player.make_move(None)
+
+    # Verifica se o resultado é o esperado
+    assert column == 3
